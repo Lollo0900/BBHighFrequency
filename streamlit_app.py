1fd22d119b4542a5c38694dd6a3df7375f71c1fe
Lollo0900 <49994270+Lollo0900@users.noreply.github.com>
2025-01-27 16:17:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,19 @@ import streamlit as st
 
 # Using object notation
 stock_list = st.sidebar.text_input(
-    "Input your portfolio Ticker", "MSFT AAPL",
+    "Input your portfolio Ticker:", "MSFT AAPL",
     placeholder="e.g. MSFT AAPL"
 )
 # Using "with" notation
 with st.sidebar:
-    timeframe = st.select_slider(
-        "Select the timeframe on which to run the strategy",
-        ('5y','1y','9mo','3mo','1mo','5d','1d')
+    start_date = st.text_input(
+        "Select the timeframe on which to run the strategy.\n Input the start date:","2023-03-01",
+        placeholder="e.g. %Y-%m-%d"
+    )
+    end_date = st.text_input(
+        "Input the end date:","2023-06-01",
+        placeholder="e.g. %Y-%m-%d"
     )
 
-df=yf.download(stock_list.split(),period=timeframe)
+df=yf.download(stock_list.split(),start=start_date,end=end_date)
 st.dataframe(df,height=100)
