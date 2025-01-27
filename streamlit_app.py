384ef3aa9c262bb0ca8f91ce8f2c5facd58d7efd
Lollo0900 <49994270+Lollo0900@users.noreply.github.com>
2025-01-27 16:34:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import yfinance as yf
 
-st.write("Here we are going to interactively benchmark a Bollinger Bands Strategy"
-         "on a chosen set of stocks")
+st.write("We are going to interactively benchmark a Bollinger Bands Strategy"
+         " on a chosen set of stocks".)
 
 import streamlit as st
 
@@ -12,7 +12,7 @@ stock_list = st.sidebar.text_input(
     placeholder="e.g. MSFT AAPL"
 )
 # Using "with" notation
-with st.sidebar:
+with (st.sidebar):
     start_date = st.text_input(
         "Select the timeframe on which to run the strategy.\n Input the start date:","2023-03-01",
         placeholder="e.g. %Y-%m-%d"
@@ -21,6 +21,13 @@ with st.sidebar:
         "Input the end date:","2023-06-01",
         placeholder="e.g. %Y-%m-%d"
     )
+    lookback=st.text_input("Input the lookback window in absolute units.","5"
+    ,placeholder="e.g. 10"
+    )
 
 df=yf.download(stock_list.replace(" ",", "),start=start_date,end=end_date,auto_adjust=False)
+st.write("Here we summarise the historical data for the chosen stocks.")
 st.dataframe(df,height=200)
+
+st.write("A first Johansen test on the first " + lookback + "data gives the following results" )
+
