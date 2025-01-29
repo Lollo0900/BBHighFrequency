276739436c91955dca7539ac3dc86be20298477a
Lollo0900 <49994270+Lollo0900@users.noreply.github.com>
2025-01-29 15:04:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,5 +108,5 @@ open_trades = strategy.open_trades
 # Checking all closed trades
 closed_trades = strategy.closed_trades
 
-st.writes(open_trades)
-st.writes(closed_trades)
+st.write(open_trades)
+st.write(closed_trades)
