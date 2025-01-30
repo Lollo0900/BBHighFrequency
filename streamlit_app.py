ea96780a6078034af7fb5d26f04f5581d18e65ae
Lollo0900 <49994270+Lollo0900@users.noreply.github.com>
2025-01-30 14:27:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,8 @@ plt.plot(spread)
 plt.grid()
 st.pyplot(fig)
 
-
+st.write("Running a Bolliger Band Stratey  with lookback "+str(lookback)+", entry Z-score "+str(entry_z)+", exit Z-score "+str(exit_z)+", on "
+        "the Adjusted Closed prices from "+start_date+" to "+end_date+",\n results in the following closed trades:")
 # Creating a strategy
 strategy = BollingerBandsTradingRule(sma_window=lookback, std_window=lookback,
                                      entry_z_score=entry_z, exit_z_score_delta=exit_z)
@@ -116,7 +117,7 @@ copy=closed_trades_df.copy()
 closed_trades_df.drop('t1',axis=0,inplace=True)
 closed_trades_df.drop('uuid',axis=0,inplace=True)
 closed_trades_df.loc['t1']=copy.loc['t1']
-st.write("Running a Bolliger Band Stratey  with lookback"+str(lookback)+",entry Z-score"+str(entry_z)+",exit Z-score"+str(exit_z)+"on "
-        "the Adjusted Closed prices from "+start_date+"to"+end_date+"\n results in the following closed trades:")
+st.write("Running a Bolliger Band Stratey  with lookback "+str(lookback)+", entry Z-score "+str(entry_z)+", exit Z-score "+str(exit_z)+", on "
+        "the Adjusted Closed prices from "+start_date+" to "+end_date+",\n results in the following closed trades:")
 st.dataframe(closed_trades_df)
 st.dataframe(open_trades_df)
