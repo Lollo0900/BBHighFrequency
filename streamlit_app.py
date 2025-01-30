e743a8c73db39a44b32df944a0d60f5abb619cc5
Lollo0900 <49994270+Lollo0900@users.noreply.github.com>
2025-01-30 14:26:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,7 +116,7 @@ copy=closed_trades_df.copy()
 closed_trades_df.drop('t1',axis=0,inplace=True)
 closed_trades_df.drop('uuid',axis=0,inplace=True)
 closed_trades_df.loc['t1']=copy.loc['t1']
-st.write("Running a Bolliger Band Stratey  with lookback"+lookback+",entry Z-score"+entry_z+",exit Z-score"+exit_z+"on "
+st.write("Running a Bolliger Band Stratey  with lookback"+str(lookback)+",entry Z-score"+str(entry_z)+",exit Z-score"+str(exit_z)+"on "
         "the Adjusted Closed prices from "+start_date+"to"+end_date+"\n results in the following closed trades:")
 st.dataframe(closed_trades_df)
 st.dataframe(open_trades_df)
